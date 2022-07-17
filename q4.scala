3518cbab080f11e527cb42b5e393411ce27bd662
DilmiHemangi <91409229+DilmiHemangi@users.noreply.github.com>
2022-07-17 22:30:24 +0700

    Update q4.scala

--- q4.scala
+++ q4.scala
@@ -1,6 +1,10 @@
 object q4 extends App{
     def bookPrice(x:Int):Double=x*24.95;
     def discount(amount:Double):Double=amount*0.4;
-    def shippingCost(x:Int):Double=3*x+(x-50)*0.75;
-    printf("Total cost = Rs.%.2f",bookPrice(60)-discount(bookPrice(60))+shippingCost(60));
+    def shippingCost(x:Int):Double= x match{
+        case x if x<=50=>3*x;
+        case x if x>50=>3*x+(x-50)*0.75;
+    }
+    def total_cost(x:Int):Double=(bookPrice(x)-discount(bookPrice(x))+shippingCost(x));
+    printf("Total cost = Rs.%.2f",total_cost(60));
 }
